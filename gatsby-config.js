c88313e7142285a605dc8b8b99452c90a0da4e58
Miguel Rivera <migue.rlopez@gmail.com>
2021-08-05 16:50:54 -0300

    Finished Gatsby Tutorial Parte 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,20 @@
 module.exports = {
   siteMetadata: {
+    description: "Trying out Gatsby for a simple portfolio",
     siteUrl: "https://www.yourdomain.tld",
-    title: "Tutorial",
+    title: "My first Gatsby Page",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
   ],
 };
