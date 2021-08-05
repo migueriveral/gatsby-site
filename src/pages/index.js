b6c35e9694c36c92f058f1dde941976ea4674a5d
Miguel Rivera <migue.rlopez@gmail.com>
2021-08-05 16:09:24 -0300

    Finished Gatsby Tutorial Parte 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>Hey! My name is Miguel and I'm a Full-Stack Web Developer!</p>
+      <StaticImage
+        alt="Awesome looking laptop with code on it"
+        src="../images/oskar-yildiz-unsplash.jpg"
+      />
     </Layout>
   )
 }
