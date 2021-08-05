b6526820f0b5ab36b25d1182da4153e18dc8aa71
Miguel Rivera <migue.rlopez@gmail.com>
2021-08-05 15:38:53 -0300

    Finished Gatsby Tutorial parte 2

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,8 +3,8 @@ import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </Layout>
   )
 }
